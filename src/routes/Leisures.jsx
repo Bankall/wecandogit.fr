9bd55bd521363ee361c91c3fd689bd1a3cc14e7b
Christian Sulecki <christian.sulecki@piximedia.com>
2025-08-14 13:48:00 +0200

    adding missing Leisures page

---
+++ src/routes/Leisures.jsx
@@ -0,0 +1,37 @@
+import { HeroLight } from "../components/HeroLight";
+import { Activity } from "../components/Activity";
+
+function Leisures() {
+	return (
+		<>
+			<HeroLight>
+				<h1>Loisirs</h1>
+			</HeroLight>
+			<section className='all-activities' id='nos-activites'>
+				<div className='content'>
+					<h2>Un espace sécurisé pour des rencontres sereines</h2>
+					<p>Le parc en collectif permet aux chiens de se rencontrer dans un cadre structuré et sécurisé, sous la supervision d’éducatrices bienveillantes. Un moment pour explorer, jouer, interagir ou tout simplement observer.</p>
+				</div>
+
+				<div className='content flex-row flex-wrap md-wrap align-normal width-50 margin-t-50 gap-50'>
+					<Activity id='9' image='/assets/medias/collectif.jpg' />
+					<Activity id='14' image='/assets/medias/chiens_calmes.jpg' />
+					<Activity id='48' image='/assets/medias/grands_gab.jpg' />
+					<Activity id='19' image='/assets/medias/DSCF7689.JPG' />
+					<Activity id='37' image='/assets/medias/DSCF1372.JPG' />
+					<Activity id='36' image='/assets/medias/DSCF9997.JPG' />
+				</div>
+
+				<div className='flex-row margin-t-50 justify-center'>
+					<div className='button small flex-no-grow'>
+						<a href={`/activites`} className='button small'>
+							Retour aux activités
+						</a>
+					</div>
+				</div>
+			</section>
+		</>
+	);
+}
+
+export { Leisures as Component };
