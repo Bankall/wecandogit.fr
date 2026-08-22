159b120447dc0830d4d235175060d33b039ca6a5
Christian Sulecki <christian.sulecki@gmail.com>
2026-08-22 14:01:04 +0200

    Mark as paid now mark it as just paid and not via stripe if not done thru stripe

--- src/components/DashboardListComponent.jsx
+++ src/components/DashboardListComponent.jsx
@@ -180,7 +180,7 @@ export default function DashboardListComponent({ type, title, addLabel, allowedA
 				{extraTitle ? " - " + extraTitle : ""}
 			</div>
 			<div className='content'>
-				{response.data?.length && (response.data?.length > 10 || params.action === "filter") ? (
+				{response.data?.length && (response.data?.length > 10 || params.action === "filter") ?
 					<div className='filter-box margin-b-20'>
 						<input
 							type='text'
@@ -201,7 +201,7 @@ export default function DashboardListComponent({ type, title, addLabel, allowedA
 							x
 						</span>
 					</div>
-				) : null}
+				:	null}
 
 				{addLabel && (
 					<Link className='margin-b-20' to={`/account/${type}/create${id_user ? `/${id_user}` : ""}`}>
@@ -209,174 +209,171 @@ export default function DashboardListComponent({ type, title, addLabel, allowedA
 					</Link>
 				)}
 
-				{response.data?.length
-					? response.data.map((item, index) => {
-							if (filter && shouldBeFiltered(filter, item)) {
-								return <div key={index}></div>;
-							}
+				{response.data?.length ?
+					response.data.map((item, index) => {
+						if (filter && shouldBeFiltered(filter, item)) {
+							return <div key={index}></div>;
+						}
 
-							return (
-								<div className='row' key={index} id={item.id}>
-									<div className='flex-row no-wrap'>
-										<span className='list-detail'>
-											{item.date ? `${formatDate(item.date)} - ` : ""}
-											{item.group_label ? (
-												<>
-													{item.date ? (
+						return (
+							<div className='row' key={index} id={item.id}>
+								<div className='flex-row no-wrap'>
+									<span className='list-detail'>
+										{item.date ? `${formatDate(item.date)} - ` : ""}
+										{item.group_label ?
+											<>
+												{item.date ?
+													<>
+														<b>{item.group_label}</b> - <Interweave content={item.label} />
+													</>
+												:	<>
+														{item.group_label} - <Interweave content={item.label} />
+													</>
+												}
+											</>
+										:	<Interweave className='flex-grow' content={item.label} />}
+									</span>
+
+									{item.email && item.id && (
+										<span>
+											<a href={`${axios.defaults.baseURL}/fake-user/${item.id}`} target='_blank'>
+												<button className='small'>Fake me</button>
+											</a>
+										</span>
+									)}
+
+									{getPaymentLabel(item) === "En attente de paiement" && isSupported(item) && allowedActions.includes("pay") && (
+										<a href={`${import.meta.env.VITE_API_ENDPOINT}/cart/stripe-redirect-no-trainer/${item.payment_details}`}>
+											<button className='smallest'>Régler</button>
+										</a>
+									)}
+
+									{typeof item.paid !== "undefined" && <span className={item.paid ? "paid" : "unpaid"}>{getPaymentLabel(item)}</span>}
+
+									{allowedActions.includes("book-reservation") && (
+										<Link to={`/account/${type}/book/${item.id}`}>
+											<button className='small'>Inscire un chien</button>
+										</Link>
+									)}
+
+									{allowedActions.includes("modify") && (
+										<Link to={`/account/${type}/edit/${item.id}`}>
+											<button className='small'>Modifier</button>
+										</Link>
+									)}
+
+									{allowedActions.includes("handleUserPackage") && (
+										<Link to={`/account/users/user-package/${item.id}`}>
+											<button className='small'>Voir les formules</button>
+										</Link>
+									)}
+
+									{allowedActions.includes("handleUserReservation") && (
+										<Link to={`/account/users/reservations/${item.id}`}>
+											<button className='small'>Voir les réservations</button>
+										</Link>
+									)}
+
+									{allowedActions.includes("marked-package-as-paid") && (
+										<button
+											className='small'
+											onClick={() => {
+												markAsPaid(item.id, "user_package", type);
+											}}>
+											Marquer comme payé
+										</button>
+									)}
+
+									{(allowedActions.includes("delete") || (allowedActions.includes("delete-24") && isNotTooLate(item.date))) && (
+										<i
+											className='fa-solid fa-trash-can'
+											aria-hidden='true'
+											style={{ color: "var(--invalid-color)", cursor: "pointer" }}
+											onClick={() => {
+												handleDelete(item.id, type);
+											}}></i>
+									)}
+								</div>
+
+								{item.dogs && item.dogs.length ?
+									<div>
+										<ul className='margin-t-10'>
+											{item.dogs.map((dog, index) => (
+												<li className='flex-row' key={index}>
+													{dog.id && (
 														<>
-															<b>{item.group_label}</b> - <Interweave content={item.label} />
-														</>
-													) : (
-														<>
-															{item.group_label} - <Interweave content={item.label} />
+															{dog.id_reservation && (
+																<i
+																	className='fa-solid fa-trash-can'
+																	aria-hidden='true'
+																	style={{ color: "var(--invalid-color)", cursor: "pointer" }}
+																	onClick={() => {
+																		handleDelete(dog.id_reservation, "reservation", "slot");
+																	}}></i>
+															)}
+
+															<Link to={`/account/dog/edit/${dog.id}`}>
+																<i className='fa-solid fa-pen' aria-hidden='true' style={{ cursor: "pointer" }}></i>
+															</Link>
 														</>
 													)}
-												</>
-											) : (
-												<Interweave className='flex-grow' content={item.label} />
-											)}
-										</span>
+													<span className='flex-grow'>&nbsp;- {dog.label}</span>
+													{typeof dog.paid !== "undefined" && <span className={dog.paid ? "paid" : "unpaid"}>{getPaymentLabel(dog)}</span>}
+													{typeof dog.paid !== "undefined" && !dog.paid && (
+														<button
+															className='smallest'
+															onClick={async () => {
+																const { data } = await axios.put(`/reservation/${dog.id_reservation}`, { paid: 1, payment_type: "later" });
+																const { id } = data;
 
-										{item.email && item.id && (
-											<span>
-												<a href={`${axios.defaults.baseURL}/fake-user/${item.id}`} target='_blank'>
-													<button className='small'>Fake me</button>
-												</a>
-											</span>
-										)}
-
-										{getPaymentLabel(item) === "En attente de paiement" && isSupported(item) && allowedActions.includes("pay") && (
-											<a href={`${import.meta.env.VITE_API_ENDPOINT}/cart/stripe-redirect-no-trainer/${item.payment_details}`}>
-												<button className='smallest'>Régler</button>
-											</a>
-										)}
-
-										{typeof item.paid !== "undefined" && <span className={item.paid ? "paid" : "unpaid"}>{getPaymentLabel(item)}</span>}
-
-										{allowedActions.includes("book-reservation") && (
-											<Link to={`/account/${type}/book/${item.id}`}>
-												<button className='small'>Inscire un chien</button>
-											</Link>
-										)}
-
-										{allowedActions.includes("modify") && (
-											<Link to={`/account/${type}/edit/${item.id}`}>
-												<button className='small'>Modifier</button>
-											</Link>
-										)}
-
-										{allowedActions.includes("handleUserPackage") && (
-											<Link to={`/account/users/user-package/${item.id}`}>
-												<button className='small'>Voir les formules</button>
-											</Link>
-										)}
-
-										{allowedActions.includes("handleUserReservation") && (
-											<Link to={`/account/users/reservations/${item.id}`}>
-												<button className='small'>Voir les réservations</button>
-											</Link>
-										)}
-
-										{allowedActions.includes("marked-package-as-paid") && (
-											<button
-												className='small'
-												onClick={() => {
-													markAsPaid(item.id, "user_package", type);
-												}}>
-												Marquer comme payé
-											</button>
-										)}
-
-										{(allowedActions.includes("delete") || (allowedActions.includes("delete-24") && isNotTooLate(item.date))) && (
-											<i
-												className='fa-solid fa-trash-can'
-												aria-hidden='true'
-												style={{ color: "var(--invalid-color)", cursor: "pointer" }}
-												onClick={() => {
-													handleDelete(item.id, type);
-												}}></i>
-										)}
+																if (id) {
+																	dispatchEvent(new Event(`refresh-list-${type}`));
+																}
+															}}>
+															Régler
+														</button>
+													)}
+												</li>
+											))}
+										</ul>
 									</div>
+								:	null}
 
-									{item.dogs && item.dogs.length ? (
-										<div>
-											<ul className='margin-t-10'>
-												{item.dogs.map((dog, index) => (
-													<li className='flex-row' key={index}>
-														{dog.id && (
-															<>
-																{dog.id_reservation && (
-																	<i
-																		className='fa-solid fa-trash-can'
-																		aria-hidden='true'
-																		style={{ color: "var(--invalid-color)", cursor: "pointer" }}
-																		onClick={() => {
-																			handleDelete(dog.id_reservation, "reservation", "slot");
-																		}}></i>
-																)}
+								{item.details && item.details.length ?
+									<div>
+										<span>Détail</span>
+										<ul className='margin-t-10'>
+											{item.details.map((detail, index) => (
+												<li className='flex-row' key={index}>
+													<span className='flex-grow'>&nbsp;- {detail.label}</span>
+													<span className='price'>
+														<b>{detail.price}€</b>
+													</span>
+												</li>
+											))}
+										</ul>
+									</div>
+								:	null}
 
-																<Link to={`/account/dog/edit/${dog.id}`}>
-																	<i className='fa-solid fa-pen' aria-hidden='true' style={{ cursor: "pointer" }}></i>
-																</Link>
-															</>
-														)}
-														<span className='flex-grow'>&nbsp;- {dog.label}</span>
-														{typeof dog.paid !== "undefined" && <span className={dog.paid ? "paid" : "unpaid"}>{getPaymentLabel(dog)}</span>}
-														{typeof dog.paid !== "undefined" && !dog.paid && (
-															<button
-																className='smallest'
-																onClick={async () => {
-																	const { data } = await axios.put(`/reservation/${dog.id_reservation}`, { paid: 1 });
-																	const { id } = data;
-
-																	if (id) {
-																		dispatchEvent(new Event(`refresh-list-${type}`));
-																	}
-																}}>
-																Régler
-															</button>
-														)}
-													</li>
-												))}
-											</ul>
-										</div>
-									) : null}
-
-									{item.details && item.details.length ? (
-										<div>
-											<span>Détail</span>
-											<ul className='margin-t-10'>
-												{item.details.map((detail, index) => (
-													<li className='flex-row' key={index}>
-														<span className='flex-grow'>&nbsp;- {detail.label}</span>
-														<span className='price'>
-															<b>{detail.price}€</b>
-														</span>
-													</li>
-												))}
-											</ul>
-										</div>
-									) : null}
-
-									{item.waiting_list && item.waiting_list.length ? (
-										<div>
-											<span>List d'attente</span>
-											<ul className='margin-t-10'>
-												{item.waiting_list.map((detail, index) => (
-													<li className='flex-row' key={index}>
-														<span className='flex-grow'>&nbsp;- {detail.label}</span>
-														<span className='price'>
-															<b>{detail.status}</b>
-														</span>
-													</li>
-												))}
-											</ul>
-										</div>
-									) : null}
-								</div>
-							);
-						})
-					: "Aucun élément pour l'instant"}
+								{item.waiting_list && item.waiting_list.length ?
+									<div>
+										<span>List d'attente</span>
+										<ul className='margin-t-10'>
+											{item.waiting_list.map((detail, index) => (
+												<li className='flex-row' key={index}>
+													<span className='flex-grow'>&nbsp;- {detail.label}</span>
+													<span className='price'>
+														<b>{detail.status}</b>
+													</span>
+												</li>
+											))}
+										</ul>
+									</div>
+								:	null}
+							</div>
+						);
+					})
+				:	"Aucun élément pour l'instant"}
 			</div>
 		</>
 	);
